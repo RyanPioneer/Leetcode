1a3a0e0d10e9ba99863325116030604d9f0ba3d1
Ryan <ryanpionee2@gmail.com>
2021-07-12 09:16:20 +0800

    2021/7/12 Min-Max Heap

--- 0001~0500/0295. Find Median from Data Stream/main.cpp
+++ 0001~0500/0295. Find Median from Data Stream/main.cpp
@@ -11,22 +11,52 @@
  */
 
 #include<iostream>
-#include<string>
+#include<queue>
 using namespace std;
 
 class MedianFinder {
 public:
+    priority_queue<int, vector<int>, greater<int>>minHeap;  //最小的優先取出
+    priority_queue<int, vector<int>, less<int>>maxHeap; //最大的優先取出
+    
     /** initialize your data structure here. */
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        
+        if(maxHeap.empty())
+            maxHeap.push(num);
+        else if(minHeap.empty() || (maxHeap.size() > minHeap.size())) {
+            if(maxHeap.top() > num) {
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+                maxHeap.push(num);
+            } else
+                minHeap.push(num);
+        } else if(maxHeap.size() < minHeap.size()) {
+            if(minHeap.top() < num) {
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+                minHeap.push(num);
+            } else
+                maxHeap.push(num);
+        } else {
+            if(maxHeap.top() > num)
+                maxHeap.push(num);
+            else
+                minHeap.push(num);
+        }
     }
     
     double findMedian() {
-        
+        if(maxHeap.size() > minHeap.size()) {
+            return maxHeap.top();
+        } else if(maxHeap.size() < minHeap.size()) {
+            return minHeap.top();
+        } else {
+            return (double)(maxHeap.top()+minHeap.top())/2;
+        }
     }
 };
 
