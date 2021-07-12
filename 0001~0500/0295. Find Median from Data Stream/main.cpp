c0d39d616cd71066145ad3141d340ee0ebbeade4
Ryan <ryanpionee2@gmail.com>
2021-07-12 08:10:22 +0800

    2021/7/12 Min-Max Heap

--- 0001~0500/0295. Find Median from Data Stream/main.cpp
+++ 0001~0500/0295. Find Median from Data Stream/main.cpp
@@ -1,7 +1,17 @@
-// Source: https://leetcode.com/problems/find-median-from-data-stream/
-// Date: 2021/7/12
+/** 
+ * Source: https://leetcode.com/problems/find-median-from-data-stream/
+ * Date: 2021/7/12
+ * Answers within 10^-5 of the actual answer will be accepted.
+ * Skill: Max-Heap and Min-Heap
+ * Skill: 使用2個Heap，分別是Max-Heap與Min-Heap。2個Heap的大小維持整個輸入整數數量的一半，
+ * Max-Heap的最大值為目前一半數量的最大值、Min-Heap的最小值為目前一半數量的最小值。
+ * 取Median時，如果2個Heap大小一樣，取2邊Top的平均值；否則取數量較多的Top。
+ * 新增數字的時間複雜度為O(logN)、取中位數的時間複雜度為O(1)
+ * Priority queue: https://dangerlover9403.pixnet.net/blog/post/197006355-%5B%E6%95%99%E5%AD%B8%5D-priority-queue
+ */
 
 #include<iostream>
+#include<string>
 using namespace std;
 
 class MedianFinder {
