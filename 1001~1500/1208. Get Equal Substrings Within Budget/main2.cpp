8593cc9cbe3f6a0be4d7abc57f573b42a15700aa
Ryan <ryanpionee2@gmail.com>
2021-07-13 21:43:14 +0800

    2021/7/13 Sliding Window

---
+++ 1001~1500/1208. Get Equal Substrings Within Budget/main2.cpp
@@ -0,0 +1,23 @@
+/** 
+ * Source: https://leetcode.com/problems/get-equal-substrings-within-budget/
+ * Date: 2021/7/13
+ * Skill: Sliding Window
+ * Constraints: 
+ *      1 <= s.length, t.length <= 10^5
+ *      0 <= maxCost <= 10^6
+ *      s and t only contain lower case English letters.
+ */
+
+#include<iostream>
+#include<string>
+#include<math.h>
+using namespace std;
+
+int equalSubstring(string s, string t, int k) {
+        int n = s.length(), i = 0, j;
+        for (j = 0; j < n; ++j) {
+            if ((k -= abs(s[j] - t[j])) < 0)
+                k += abs(s[i] - t[i++]);
+        }
+        return j - i;
+    }
