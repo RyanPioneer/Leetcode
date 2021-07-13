e9c1bf4c48704c3a048567ca097ad20f61ead464
Ryan <ryanpionee2@gmail.com>
2021-07-13 12:14:32 +0800

    2021/7/13 DP

--- 0001~0500/0022. Generate Parentheses/main2.cpp
+++ 0001~0500/0022. Generate Parentheses/main2.cpp
@@ -3,9 +3,14 @@
  * Date: 2021/7/13
  * Skill: DP
  * 1 <= n <= 8
- * Memory Search
+ * Skill: DP
+ * Reference: https://stackoverflow.com/questions/56475430/closure-number-method-for-generate-parenthesis-problem
  * Time Complexity: O(2^n)
  * Space Complexity: O(2^n)
+ * For each closure number c, we know the starting and 
+ * ending brackets must be at index 0 and 2*c + 1. 
+ * Then, the 2*c elements between must be a valid sequence, 
+ * plus the rest of the elements must be a valid sequence.
  */
 
 #include<iostream>
