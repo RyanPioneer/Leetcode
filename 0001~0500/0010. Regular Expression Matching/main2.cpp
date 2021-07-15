87664f682aa50ae3a5ec5ebae3cb557d67df33f9
Ryan <ryanpionee2@gmail.com>
2021-07-15 16:01:27 +0800

    2021/7/15 DP

--- 0001~0500/0010. Regular Expression Matching/main2.cpp
+++ 0001~0500/0010. Regular Expression Matching/main2.cpp
@@ -13,8 +13,8 @@
  * 1. dp[i][j] = dp[i - 1][j - 1], if p[j - 1] != '*' && 
  * (s[i - 1] == p[j - 1] || p[j - 1] == '.');
  * 2. dp[i][j] = dp[i][j - 2], if p[j - 1] == '*' and the pattern repeats for 0 time;
- * 3. dp[i][j] = dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'), 
- * 4. if p[j - 1] == '*' and the pattern repeats for at least 1 time.
+ * 3. dp[i][j] = dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.')
+ * ,if p[j - 1] == '*' and the pattern repeats for at least 1 time.
  */
 
 #include<iostream>
