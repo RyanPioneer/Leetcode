67d09471527974d5657094fe3af060d61eaf7d12
Ryan <ryanpionee2@gmail.com>
2021-07-17 23:03:49 +0800

    2021/7/17 Easy Hash Table

--- 0001~0500/0217. Contains Duplicate/main.cpp
+++ 0001~0500/0217. Contains Duplicate/main.cpp
@@ -1,19 +1,32 @@
 /** 
  * Source: https://leetcode.com/problems/contains-duplicate/
- * Date: 2021/7/13
+ * Date: 2021/7/17
+ * Skill: Hash Table
+ * * Internally unordered_map is implemented using Hash Table
+ * Runtime: 33 ms, faster than 52.31% of C++ online submissions
+ * Memory Usage: 20 MB, less than 71.44% of C++ online submissions
+ * Time complexity: O(n)
+ * Space complexity: O(n)
  * Constraints: 
- *      1 <= nums.length <= 10^5
- *      -10^9 <= nums[i] <= 10^9
+ * 
  */
 
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
 class Solution {
 public:
-    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int>myMap;
+        for (auto i: nums) {
+            if (myMap.count(i))
+                return true;
+            else
+                myMap[i] = 1;
+        }
+        return false;
     }
 };
 
