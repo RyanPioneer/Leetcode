4ecd4abab9068172f11a8b11af8b730c72833ab7
Ryan <ryanpionee2@gmail.com>
2021-07-18 14:06:13 +0800

    2021/7/18 Medium Sliding Window

--- 0001~0500/0220. Contains Duplicate III/main.cpp
+++ 0001~0500/0220. Contains Duplicate III/main.cpp
@@ -1,27 +1,41 @@
 /** 
  * Source: https://leetcode.com/problems/contains-duplicate-iii/
- * Date: 2021/7/13
+ * Date: 2021/7/18
  * Skill: Sliding Window
+ * Runtime: 32 ms, faster than 79.89% of C++ online submissions
+ * Memory Usage: 18.8 MB, less than 23.11% of C++ online submissions
+ * Time complexity:
+ * Space complexity: 
  * Constraints: 
  *      0 <= nums.length <= 2 * 10^4
- *      -2^31 <= nums[i] <= 2^31 - 1
- *      0 <= k <= 10^4
- *      0 <= t <= 2^31 - 1
+ * Goal: find two distinct indices i and j in the array 
+ *      such that abs(nums[i] - nums[j]) <= t and abs(i - j) <= k.
  */
-
 #include<iostream>
 #include<vector>
+#include<set>
 using namespace std;
 
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        
+        set<long> mySet;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > k)
+                mySet.erase(nums[i-k-1]);
+            auto j = mySet.lower_bound((long)nums[i] - (long)t);    // * Note!!
+            if (j != mySet.end() && *j - nums[i] <= t)
+                return true;
+            mySet.insert((long)nums[i]);
+        }
+        return false;
     }
 };
 
 int main() {
-  
+    vector<int> vec = {1,5,9,1,5,9};
+    Solution s;
+    cout<<s.containsNearbyAlmostDuplicate(vec, 2, 3);
 }
 
 static const auto io_sync_off = []() {
