5fcde76b5268a675b341e8ec506d32cb713d95fa
RyanPionee2 <ryanpionee2@gmail.com>
2023-01-06 17:28:13 +0800

    2023/1/6

---
+++ 0501~1000/0731. My Calendar II/main.py
@@ -0,0 +1,28 @@
+"""
+Source: https://leetcode.com/problems/my-calendar-ii/
+Date: 2023/1/6
+Skill:
+Ref:
+Runtime: 175 ms, faster than 42.47%
+Memory Usage: 20.1 MB, less than 22.49%
+Time complexity:
+Space complexity:
+Constraints:
+    0 <= start < end <= 10^9
+"""
+
+from typing import List, Optional
+
+
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.calendar = []
+
+    def book(self, start: int, end: int) -> bool:
+
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
