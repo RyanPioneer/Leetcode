a1806555c9ec8ea5ee12462dae709b9021d1540a
RyanPionee2 <ryanpionee2@gmail.com>
2023-02-05 13:08:57 +0800

    2023/2/5

--- 0001~0500/0032. Longest Valid Parentheses/main2.py
+++ 0001~0500/0032. Longest Valid Parentheses/main2.py
@@ -2,8 +2,8 @@
 Source: https://leetcode.com/problems/longest-valid-parentheses/
 Date: 2023/2/1
 Skill: Dynamic Programming
-Runtime: 43 ms, faster than 84.22%
-Memory Usage: 14.6 MB, less than 60.68%
+Runtime: 44 ms, faster than 79.68%
+Memory Usage: 14.2 MB, less than 74.62%
 Time complexity:
 Space complexity:
 Constraints:
@@ -21,3 +21,22 @@ import functools
 
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
+        # special case
+        if len(s) < 2:
+            return 0
+        # base case
+        dp = [0] * len(s)
+        if s[0] == "(" and s[1] == ")":
+            dp[1] = 2
+        # recursive solution
+        for i in range(2,len(s)):
+            if s[i] == ")":
+                if s[i-1] == "(":
+                    dp[i] = dp[i-2] + 2
+                if s[i-1] == ")" and (i - dp[i-1] - 1) >= 0 and s[i - dp[i-1] - 1] == "(":
+                    if i - dp[i-1] - 2 >= 0:
+                        dp[i] = dp[i-1] + 2 + dp[i - dp[i-1] - 2]
+                    else:
+                        dp[i] = dp[i-1] + 2
+        # result
+        return max(dp)
