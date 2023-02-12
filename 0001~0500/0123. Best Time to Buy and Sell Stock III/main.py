a400b005aee09af8ca49bda711a73862e485b197
RyanPionee2 <ryanpionee2@gmail.com>
2023-02-12 17:15:00 +0800

    2023/2/12

--- 0001~0500/0123. Best Time to Buy and Sell Stock III/main.py
+++ 0001~0500/0123. Best Time to Buy and Sell Stock III/main.py
@@ -1,13 +1,14 @@
 """
 Source: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
-Date: 2023/2/11
+Date: 2023/2/12
 Skill: Dynamic Programming
-Runtime: 1383 ms, faster than 49.87%
-Memory Usage: 28.5 MB, less than 42.93%
+Runtime: 1168 ms, faster than 77.74%
+Memory Usage: 28.9 MB, less than 35.79%
 Time complexity:
 Space complexity:
 Constraints:
-
+    1 <= prices.length <= 10^5
+    0 <= prices[i] <= 10^5
 """
 
 import math
@@ -21,8 +22,12 @@ import functools
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        hold1, release1, hold2, release2 = -prices[0], 0, -math.inf, 0
+        for price in prices:
+            hold1, release1, hold2, release2 = max(hold1, -price), max(release1, hold1 + price),\
+                max(hold2, release1-price), max(release2, hold2 + price)
 
-
+        return max(release1, release2)
 
 
 if __name__ == "__main__":
