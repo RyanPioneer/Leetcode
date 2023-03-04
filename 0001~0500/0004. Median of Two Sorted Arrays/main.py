d7e2f8a13de668b53ac3a4192fc7ddc25fda6f19
ryanpionee2 <ryanpionee2@gmail.com>
2023-03-04 12:16:51 +0800

    2023/3/4

--- 0001~0500/0004. Median of Two Sorted Arrays/main.py
+++ 0001~0500/0004. Median of Two Sorted Arrays/main.py
@@ -1,18 +1,44 @@
 """
 Source: https://leetcode.com/problems/median-of-two-sorted-arrays/
-Date: 2022/2/18
-Constraints: 
-    nums1.length == m
-    nums2.length == n
-    0 <= m <= 1000
-    0 <= n <= 1000
-    1 <= m + n <= 2000
-    -10^6 <= nums1[i], nums2[i] <= 10^6
-    !The overall run time complexity should be O(log (m+n)).
+Date: 2023/3/4
+Skill:
+Runtime: 100 ms, faster than 47.47%
+Memory Usage: 14.2 MB, less than 16.78%
+Time complexity:
+Space complexity:
+Constraints:
+
 """
 
-from typing import List
+import math
+from typing import List, Optional
+from collections import defaultdict, deque
+from heapq import heapify, heappush, heappop, nsmallest
+from bisect import bisect_left, bisect_right
+import heapq
+import functools
+
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        
+        length1, length2 = len(nums1), len(nums2)
+
+        def FindKthSmallest(arr1, start1, len1, arr2, start2, len2, k):
+            if len1 - start1 > len2 - start2:
+                return FindKthSmallest(arr2, start2, len2, arr1, start1, len1, k)
+            if start1 == len1:
+                return arr2[start2 + k - 1]
+            if k == 1:
+                return min(arr1[start1], arr2[start2])
+            k1 = min(k // 2, len1 - start1)
+            k2 = k - k1
+            if arr1[start1 + k1 - 1] > arr2[start2 + k2 - 1]:
+                return FindKthSmallest(arr1, start1, len1, arr2, start2 + k2, len2, k - k2)
+            else:
+                return FindKthSmallest(arr1, start1 + k1, len1, arr2, start2, len2, k - k1)
+
+        if (length1 + length2) % 2 == 1:
+            return FindKthSmallest(nums1, 0, length1, nums2, 0, length2, (length1 + length2 + 1) // 2)
+        else:
+            return (FindKthSmallest(nums1, 0, length1, nums2, 0, length2, (length1 + length2) // 2)
+                    + FindKthSmallest(nums1, 0, length1, nums2, 0, length2, (length1 + length2) // 2 + 1)) / 2
