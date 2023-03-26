e8b0e4f2ed87a3cd221caff51037f2288477d30d
ryanpionee2 <ryanpionee2@gmail.com>
2023-03-26 15:28:49 +0800

    2023/3/26

--- 1501~2000/1883. Minimum Skips to Arrive at Meeting On Time/main.py
+++ 1501~2000/1883. Minimum Skips to Arrive at Meeting On Time/main.py
@@ -1,13 +1,13 @@
 """
 Source: https://leetcode.com/problems/minimum-skips-to-arrive-at-meeting-on-time/
-Date: 2023/3/25
+Date: 2023/3/26
 Skill: Dynamic Programming
 Runtime: 2992 ms, faster than 76.39%
 Memory Usage: 28 MB, less than 94.85%
 Time complexity:
 Space complexity:
 Constraints:
-
+    1 <= dist.length <= 1000
 """
 
 import math
@@ -20,8 +20,21 @@ import functools
 
 class Solution:
     def minSkips(self, dist: List[int], speed: int, hoursBefore: int) -> int:
+        n, dp = len(dist), [[1e10 for _ in range(len(dist))] for _ in range(len(dist))]
+        dp[0][0], eps = 0, 1e-9  # * Because of precision issues
+        for i in range(1, n):
+            dp[i][0] = math.ceil(dp[i-1][0] + dist[i-1] / speed - eps)
+            for j in range(1, i+1):
+                dp[i][j] = min(math.ceil(dp[i-1][j] + dist[i-1] / speed - eps),
+                               dp[i-1][j-1] + dist[i-1] / speed)
 
+        for i in range(n):
+            if dp[n-1][i] + dist[n-1] / speed <= hoursBefore:
+                return i
+        return -1
 
 
 if __name__ == "__main__":
     s = Solution()
+    dist = [7, 3, 5, 5]
+    res = s.minSkips(dist, 2, 10)
