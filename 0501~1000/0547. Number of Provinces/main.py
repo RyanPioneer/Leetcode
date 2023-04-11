81d8f8472dcb0a27856dfc2a8ab95c397ab0bb90
ryanpionee2 <ryanpionee2@gmail.com>
2023-04-11 22:38:02 +0800

    2023/4/11

--- 0501~1000/0547. Number of Provinces/main.py
+++ 0501~1000/0547. Number of Provinces/main.py
@@ -3,8 +3,8 @@ Source: https://leetcode.com/problems/number-of-provinces/
 Date: 2023/4/11
 Skill: Union_Find
 Ref:
-Runtime: 182 ms, faster than 67.56%
-Memory Usage: 14 MB, less than 97.47%
+Runtime: 190 ms, faster than 61.43%
+Memory Usage: 14.1 MB, less than 99.77%
 Time complexity:
 Space complexity:
 Constraints:
@@ -21,7 +21,27 @@ import functools
 
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        father, sz, res = [i for i in range(len(isConnected))], len(isConnected), set()
 
+        def find_father(i):
+            if father[i] != i:
+                father[i] = find_father(father[i])
+            return father[i]
+
+        def union(x, y):
+            x_father, y_father = find_father(x), find_father(y)
+            if x_father != y_father:
+                father[max(x_father, y_father)] = min(x_father, y_father)
+
+        for i in range(sz):
+            for j in range(sz):
+                if isConnected[i][j] == 1 and i != j:
+                    union(i, j)
+
+        for i in range(sz):
+            res.add(find_father(i))
+
+        return len(res)
 
 
 if __name__ == "__main__":
---
+++ 2001~2500/2390. Removing Stars From a String/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,17 @@
+
+\
+,.idea/inspectionProfiles/Project_Default.xml,4/9/496a238a6afa168dbaf6efd37bb459331589579c
+^
+..idea/inspectionProfiles/profiles_settings.xml,1/e/1e9075f5bf079c01ef2c910709e91a497d262080
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
+8
+main2.py,c/c/cc7b753f568727eee1a611f410e85ecaa3a34e66
+8
+main3.py,3/5/355b4fe19acbaccb2562197f1c9ce73c58bfd620
+7
+main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
+8
+main4.py,3/9/39ba8962a6818bac34e84880a7740583f4369984
