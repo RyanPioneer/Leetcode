c6b2a267f83aeef560bc7df88a35ccf41e30e8cd
ryanpionee2 <ryanpionee2@gmail.com>
2023-04-22 21:53:26 +0800

    2023/4/22

--- 2501~3000/2642. Design Graph With Shortest Path Calculator/main.py
+++ 2501~3000/2642. Design Graph With Shortest Path Calculator/main.py
@@ -1,14 +1,14 @@
 """
 Source: https://leetcode.com/problems/design-graph-with-shortest-path-calculator/
 Date: 2023/4/22
-Skill:
+Skill: Floyd–Warshall algorithm
 Ref:
-Runtime: 177 ms, faster than 40.65%
-Memory Usage: 14.9 MB, less than 53.27%
+Runtime: 7271 ms, faster than 6.20%
+Memory Usage: 17.1 MB, less than 76.27%
 Time complexity:
 Space complexity:
 Constraints:
-
+    1 <= n <= 100
 """
 
 import math
@@ -23,10 +23,26 @@ from bisect import bisect_left, bisect_right
 class Graph:
 
     def __init__(self, n: int, edges: List[List[int]]):
+        self.dp, self.n = [[math.inf for _ in range(n)] for _ in range(n)], n
+        for i in range(n):
+            self.dp[i][i] = 0
+        for edge in edges:
+            self.dp[edge[0]][edge[1]] = edge[2]
+
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    self.dp[j][k] = min(self.dp[j][k], self.dp[j][i] + self.dp[i][k])
 
     def addEdge(self, edge: List[int]) -> None:
+        if edge[2] < self.dp[edge[0]][edge[1]]:
+            self.dp[edge[0]][edge[1]] = edge[2]
+            for i in range(self.n):
+                for j in range(self.n):
+                    self.dp[i][j] = min(self.dp[i][j], self.dp[i][edge[0]] + edge[2] + self.dp[edge[1]][j])
 
     def shortestPath(self, node1: int, node2: int) -> int:
+        return self.dp[node1][node2] if self.dp[node1][node2] < math.inf else -1
 
 
 # Your Graph object will be instantiated and called as such:
