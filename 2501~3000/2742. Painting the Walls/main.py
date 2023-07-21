2e116d23211af4c4cc316e436095b17edd86e649
RyanPionee2 <ryanpionee2@gmail.com>
2023-07-21 23:53:14 +0800

    2023/7/21

--- 2501~3000/2742. Painting the Walls/main.py
+++ 2501~3000/2742. Painting the Walls/main.py
@@ -1,10 +1,10 @@
 """
 Source: https://rb.gy/ftxv2
 Date: 2023/7/21
-Skill:
+Skill: dp更新過程為何從現在推到未來？因為無法窮舉之前的可能
 Ref:
-Runtime: 2669 ms, faster than 41.45%
-Memory Usage: 25.6 MB, less than 42.38%
+Runtime: 7402 ms, faster than 5.07%
+Memory Usage: 24.87 MB, less than 62.98%
 Time complexity:
 Space complexity:
 Constraints:
@@ -22,10 +22,22 @@ from bisect import bisect_left, bisect_right
 
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
-        sz = len(cost)
+        cost.insert(0, 0)
+        time.insert(0, 0)
+        sz, offset, res = len(cost), len(cost) + 1, sys.maxsize
+        dp = [[sys.maxsize for _ in range(2 * offset)] for _ in range(sz)]
+        dp[0][offset] = 0
+        for i in range(0, sz - 1):
+            for j in range(-sz, sz + 1):
+                dp[i + 1][j - 1 + offset] = min(dp[i + 1][j - 1 + offset], dp[i][j + offset])
+                jj = min(j + time[i + 1], sz)
+                dp[i + 1][jj + offset] = min(dp[i + 1][jj + offset], dp[i][j + offset] + cost[i + 1])
 
+        for i in range(sz + 1):
+            res = min(res, dp[-1][offset + i])
+
+        return res
 
 
 if __name__ == "__main__":
     s = Solution()
-
