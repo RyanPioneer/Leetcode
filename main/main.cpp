75a675d452c7d4dfc360011b6f151d78559451bc
RyanPionee2 <ryanpionee2@gmail.com>
2023-08-31 12:50:55 +0800

    2023/8/31

---
+++ main/main.cpp
@@ -0,0 +1,48 @@
+/**
+ * Source: https://rb.gy/6zmo5
+ * Date: 2023/8/31
+ * Skill:
+ * Runtime: 0 ms, faster than 100% of C++ online submissions
+ * Memory Usage: 7.92 MB, less than 15.42% of C++ online submissions
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ * Constraints:
+ *
+ */
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <deque>
+
+using namespace std;
+
+#define ll long long
+// using ll = long long;
+
+typedef pair<int, int> pairs;
+
+class Solution {
+public:
+    bool isGoodArray(vector<int>& nums) {
+        
+    }
+};
+
+static const auto io_sync_off = []() {
+    // turn off sync
+    std::ios::sync_with_stdio(false);
+    // untie in/out streams
+    std::cin.tie(nullptr);
+    return nullptr;
+}();
+
+int main() {
+    Solution s;
+    vector<int> arr{3,2,2,4,3};
+    int target = 3;
+
+    cout << target << endl;
+}
