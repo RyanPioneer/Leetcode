78daa3747ef5e150a11b568222901cce20dca879
RyanPionee2 <ryanpionee2@gmail.com>
2025-07-29 13:29:48 +0800

    2025/7/29

---
+++ 3501~4000/3628. Maximum Number of Subsequences After One Inserting/main.cpp
@@ -0,0 +1,35 @@
+/**
+ * Source: t.ly/50zUf
+ * Date: 2025/7/29
+ * Skill:
+ * Ref:
+ * Runtime: 106 ms, faster than 100.00% of C++ online submissions
+ * Memory Usage: 242.31 MB, less than 100.00% of C++ online submissions
+ * Time complexity:
+ * Space complexity:
+ * Constraints:
+ *
+ */
+
+#include <algorithm>
+#include <bitset>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+#define ll long long
+const int MOD = 1e9 + 7;
+
+class Solution {
+   public:
+    long long numOfSubsequences(string s) { ll n = s.size(); }
+};
